7a446f3d643fe759fd464e3212ddc2d1494a7eb3
Dmitry Nuzhin <iruirc@gmail.com>
2023-11-27 13:44:19 +0300

    Update swift-tools-version to 5.9 for conform comlipe dependencies.package(url: exact:)

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -20,7 +20,10 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "RxStoreKit",
-            dependencies: ["RxSwift", "RxCocoa"]),
+            dependencies: [
+                .product(name: "RxSwift", package: "RxSwift"),
+                .product(name: "RxCocoa", package: "RxSwift")
+            ]),
         .testTarget(
             name: "RxStoreKitTests",
             dependencies: ["RxStoreKit"]),
